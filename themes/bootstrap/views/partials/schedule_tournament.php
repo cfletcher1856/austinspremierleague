c80f18cd9840386361465514c43db7f6212829dd
Colin Fletcher <cfletcher1856@gmail.com>
2013-06-19 11:38:33 -0500

    Adding partial to schedule for the tournament

---
+++ themes/bootstrap/views/partials/schedule_tournament.php
@@ -0,0 +1,24 @@
+<tr class="info">
+    <td colspan="6" class="center">Top 8 Players Qualify for the Seeded Tournament</td>
+</tr>
+<tr>
+    <td rowspan="4">12 - 6/3/2013</td>
+    <td>Quarters</td>
+    <td class="center">#1 Seed vs #8 Seed</td>
+    <td class="center">#4 Seed vs #5 Seed</td>
+    <td class="center">#3 Seed vs #6 Seed</td>
+    <td class="center">#2 Seed vs #7 Seed</td>
+</tr>
+<tr>
+    <td>Semis</td>
+    <td colspan="2" class="center">Winner Board 2 vs Winner Board 3</td>
+    <td colspan="2" class="center">Winner Board 4 vs Winner Board 5</td>
+</tr>
+<tr>
+    <td>Consol.</td>
+    <td colspan="4" class="center">Loser Board 2 vs Loser Board 4</td>
+</tr>
+<tr>
+    <td>Finals</td>
+    <td colspan="4" class="center">Winner Board 2 vs Winner Board 4</td>
+</tr>
